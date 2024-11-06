4e6f2c358e112e951e99255dc101660bd128d197
Jatinpandit001 <Modhgilljatin@gmail.com>
2024-11-06 12:11:22 +0530

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-console.log("Website loaded successfully!");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 8080;
+
+// Serve static files from src directory
+app.use(express.static(path.join(__dirname, "../src")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
